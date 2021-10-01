10656426f31810bbb6d4cff059377177d24a514d
utsabk <kharelutsab@gmail.com>
2021-10-02 00:21:46 +0300

    Authentication with Jsonwebtoken  - Add signup and signin  - sign a jwt for a signed in user  - save token in cookies  - verify user with token from cookies

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
-import 'dotenv/config.js'
+import 'dotenv/config.js';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const Users = [];
+
+
+app.post('/signup', (req, res) => {
+  const body = req.body;
+
+  if (!body.email || !body.password) {
+    res.status(400).send('Invalid details');
+  } else if (Users.filter((user) => user.email === body.email).length > 0) {
+    res.status('409').send({
+      message: 'User Already Exists! Login or choose another user email',
+    });
+  } else {
+    const newUser = { email: req.body.email, password: req.body.password };
+
+    Users.push(newUser);
+
+    res.json(Users);
+  }
+});
+
+app.post('/signin', (req, res) => {
+  const body = req.body;
+
+  if (!body.email || !body.password) {
+    res.status(403).send({ message: 'Please enter both email and password' });
+  }
+  const [currentUser] = Users.filter(
+    (user) => user.email === body.email && user.password === body.password
+  );
+  if (!currentUser) res.status(404).send({ message: 'No user exists' });
+  else {
+    const token = jwt.sign(currentUser, process.env.SECRET_KEY, {
+      expiresIn: 60 * 15,
+    });
+
+    res.cookie('auth', token, { maxAge: 900000 });
+
+    res.send({ message: 'Successfully signed in' });
+  }
+});
+
+const checkSignIn = (req, res, next) => {
+  const token = req.cookies.auth;
+  console.log('token', token);
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+      err && res.status(403).send('Error');
+      console.log('Data.-', data);
+      next();
+    });
+  }
+};
+
+app.get('/', checkSignIn,(req, res) => {
   res.send('Hello World');
 });
 
