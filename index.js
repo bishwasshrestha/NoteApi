b3b348399efb1bc7465d129b990a4e2d4e70d0dc
utsabk <kharelutsab@gmail.com>
2021-10-03 13:37:54 +0300

    Remove cookies  jwt is not saved in cookies rather sent in every request

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import 'dotenv/config.js';
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const Users = [];
 
+const generateId = (list) => {
+  const maxId = list.length > 0 ? Math.max(...list.map((n) => n.user_id)) : 0;
+  return maxId + 1;
+};
 
 app.post('/signup', (req, res) => {
   const body = req.body;
@@ -22,11 +24,20 @@ app.post('/signup', (req, res) => {
       message: 'User Already Exists! Login or choose another user email',
     });
   } else {
-    const newUser = { email: req.body.email, password: req.body.password };
+    const userId = generateId(Users);
+    const newUser = {
+      user_id: userId,
+      email: body.email,
+      password: body.password,
+      time_Created: new Date(),
+    };
 
     Users.push(newUser);
 
-    res.json(Users);
+    res.status(201).send({
+      message: 'User created successfully',
+      user_id: userId,
+    });
   }
 });
 
@@ -34,37 +45,47 @@ app.post('/signin', (req, res) => {
   const body = req.body;
 
   if (!body.email || !body.password) {
-    res.status(403).send({ message: 'Please enter both email and password' });
+    res.status(403).send({ Error: 'Please enter both email and password' });
   }
   const [currentUser] = Users.filter(
     (user) => user.email === body.email && user.password === body.password
   );
-  if (!currentUser) res.status(404).send({ message: 'No user exists' });
-  else {
+
+  if (!currentUser) {
+    res.status(404).send({ Error: 'No user exists' });
+  } else {
     const token = jwt.sign(currentUser, process.env.SECRET_KEY, {
       expiresIn: 60 * 15,
     });
+    delete currentUser.password;
 
-    res.cookie('auth', token, { maxAge: 900000 });
-
-    res.send({ message: 'Successfully signed in' });
+    const successResponse = {
+      message: 'Logged in successfully',
+      token: token,
+      user: currentUser,
+    };
+    res.status(200).send(successResponse);
   }
 });
 
-const checkSignIn = (req, res, next) => {
-  const token = req.cookies.auth;
-  console.log('token', token);
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers.authorization;
 
-  if (token) {
-    jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
-      err && res.status(403).send('Error');
-      console.log('Data.-', data);
-      next();
-    });
+  if (bearerHeader) {
+    const [type, bearerToken] = bearerHeader.split(' ');
+
+    bearerToken &&
+      jwt.verify(bearerToken, process.env.SECRET_KEY, (err, data) => {
+        err && res.status(403).send({ Error: err.message });
+        console.log('Data.-', data);
+        next();
+      });
+  } else {
+    res.status(403).send({ message: 'Not authorized' });
   }
 };
 
-app.get('/', checkSignIn,(req, res) => {
+app.get('/', verifyToken, (req, res) => {
   res.send('Hello World');
 });
 
